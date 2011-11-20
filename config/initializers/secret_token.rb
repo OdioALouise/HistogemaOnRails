4f30526632a7a35df6ee0cee1507776698b583a9
Sebastian <sebastiandaloia@gmail.com>
2011-11-20 13:15:21 -0300

    Primer comiteo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HistogemaOnRails::Application.config.secret_token = 'b6ead4fd6e3cf9620a1df17159dc132ddf054e2dd8f9badcab37ded28401ac9d1a9821e78365945469b18d7c0c62c043e9fefbe7c5655064e48be4f85ba3a759'
